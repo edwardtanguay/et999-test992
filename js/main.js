0ed0bc7d6ffd8b5e872f3de9e8a074d9b3fb419b
Edward Tanguay (windows) <edward@tanguay.info>
2023-07-11 11:29:04 +0200

    click works

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const rootElem = document.querySelector('#root');
+const btnChangeColorElem = document.querySelector('.btnChangeColor');
+
+// rootElem.innerHTML = 'nnn';
+// rootElem.style.backgroundColor = 'yellow';
+
+btnChangeColorElem.addEventListener('click', () => {
+	rootElem.style.backgroundColor = 'yellow';
+})
+
+const url = 'https://edwardtanguay.vercel.app/share/techBooks.json';
+
+(async () => {
+	const response = await fetch(url);
+	const techBooks = await response.json();
+	console.log(techBooks);
+	rootElem.innerHTML = `
+		<style>
+			ul {
+				// background-color: black;
+			}
+		</style>
+		<h2>There are ${techBooks.length} tech books</h2>
+		<ul>
+		${techBooks.map(techBook => `<li>${techBook.title}</li>`).join('')}
+		</ul>
+		`;
+})();
